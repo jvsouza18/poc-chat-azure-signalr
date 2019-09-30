cd3270935624917e5aebb19d49cc64014d995939
João Souza <joao.souza@modalgr.com.br>
2019-09-30 00:00:26 -0300

    adding project signalr

---
+++ Signal-R-Hub/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Signal_R_Hub.Controllers
+{
+    [Produces("aplication/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private IHubContext<NotifyHub, ITypedHubClient> _hubContext;
+
+        public MessageController(IHubContext<NotifyHub, ITypedHubClient> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public string Post([FromBody]Message msg)
+        {
+            string retMesssage = string.Empty;
+            try
+            {
+                _hubContext.Clients.All.BroadcastMessage(msg.Type, msg.Payload);
+                retMesssage = "Success";
+            }
+            catch(Exception e)
+            {
+                retMesssage = e.ToString();
+            }
+            return retMesssage;
+        }
+    }
+}
